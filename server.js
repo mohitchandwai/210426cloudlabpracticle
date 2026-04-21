7a1860d19d5b7ec493b4c3298b163eeda3cf7bc2
Mohit chandwani <chandwanimohit87m@gmail.com>
2026-04-21 11:04:09 +0530

    Initial commit with Dockerfile

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, Node.js App Running 🚀');
+});
+
+app.get('/api/data', (req, res) => {
+    res.json({
+        message: 'This is sample API data',
+        status: 'success'
+    });
+});
+
+// Server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
